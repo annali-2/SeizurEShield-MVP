60cf428c14faee5702535a3da7911aa8969c95f4
Arshia <arshiasharma@berkeley.edu>
2024-07-27 17:21:32 -0700

    updated css & code formatting

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import pandas as pd
 import altair as alt
 import random
+
 alt.data_transformers.disable_max_rows()
 
 from flask import (
@@ -18,19 +19,20 @@ from flask import (
     url_for,
     flash,
     send_from_directory,
-    session
+    session,
 )
 from werkzeug.utils import secure_filename
 
 from preprocessing.preprocess import EEGMontage
 
 
-# Configure Flask App 
+# Configure Flask App
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "uploads/"
 app.config["DATA_FOLDER"] = "data/"
 app.config["ALLOWED_EXTENSIONS"] = {"edf"}
 app.secret_key = os.urandom(24)  # Set a unique and secret key
+
 
 # Safeguard against malicious file uplaods
 def allowed_file(filename):
@@ -45,6 +47,7 @@ def extract_model(archive_path, extract_path):
     with tarfile.open(archive_path, "r:gz") as tar:
         tar.extractall(path=extract_path)
     return extract_path
+
 
 # Load Model
 def load_model(model_path, input_size, hidden_size, output_size, num_layers):
@@ -73,35 +76,40 @@ class RNNLSTM(nn.Module):
         out = self.fc(out[:, -1, :])
         return out
 
+
 # Home Page Route
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 # Upload File Page
 @app.route("/upload")
-def upload_egg():
+def upload_eeg():
     return render_template("uploads.html")
 
-# Run Prediction 
+
+# Run Prediction
 @app.route("/predict")
 def predict_eeg():
     return render_template("predict.html")
 
-# Process File for EGG stats/description 
+
+# Process File for EGG stats/description
 def process_file(filepath):
-    # Add your .edf file processing logic using MNE-Python
     raw = mne.io.read_raw_edf(filepath)
     info = raw.info
     channels = info["ch_names"]
     duration = raw.times[-1]
     return {"channels": channels, "duration": duration}
 
-montages = ['01_tcp_ar', '02_tcp_le', '03_tcp_ar_a', '04_tcp_le_a']
 
-@app.route('/upload_file', methods=['GET', 'POST'])
+montages = ["01_tcp_ar", "02_tcp_le", "03_tcp_ar_a", "04_tcp_le_a"]
+
+
+@app.route("/upload_file", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == "POST":
         if "file" not in request.files:
             flash("No file part")
             return redirect(request.url)
@@ -111,10 +119,12 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+
             # Ensure the uploads directory exists
             os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            session['uploaded_file'] = filename  # Store the filename in session
+            session["uploaded_file"] = filename  
+
             # Process the .edf file and generate output using MNE-Python
             output = process_file(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return render_template(
@@ -127,8 +137,8 @@ def upload_file():
         else:
             flash("File type not allowed")
             return redirect(request.url)
-    else:  # GET request
-        filename = session.get('uploaded_file')
+    else: 
+        filename = session.get("uploaded_file")
         if filename:
             filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             if os.path.exists(filepath):
@@ -142,19 +152,19 @@ def upload_file():
                 )
             else:
                 flash("File not found")
-                return redirect(url_for('upload_eeg'))
+                return redirect(url_for("upload_eeg"))
         else:
             flash("No file uploaded yet")
-            return redirect(url_for('upload_eeg'))
+            return redirect(url_for("upload_eeg"))
 
-# Run Model Prediction 
+
+# Run Model Prediction
 @app.route("/predict_file", methods=["GET", "POST"])
 def predict_file():
     if request.method == "POST":
         uploaded_files = os.listdir("uploads")
         additional_info = []
 
-        # Extract the model archive
         archive_path = "model.tar.gz"
         extract_path = "./model_dir"
         extract_model(archive_path, extract_path)
@@ -169,8 +179,8 @@ def predict_file():
         # Load the model
         model = load_model(model_path, input_size, hidden_size, output_size, num_layers)
 
-        selected_montage = request.form.get('montage')
-        session['selected_montage'] = selected_montage
+        selected_montage = request.form.get("montage")
+        session["selected_montage"] = selected_montage
 
         # loop through the files but the assumption for now is that there is only one file
         for file in uploaded_files:
@@ -181,43 +191,70 @@ def predict_file():
             if selected_montage in eeg_montage.montage_dict:
                 montage = eeg_montage.montage_dict[selected_montage]
             else:
-                raise ValueError(f"Selected montage '{selected_montage}' is not available")
-            edf_preprocessed = eeg_montage.compute_differential_signals(edf_csv, montage)
-            edf_preprocessed2 = edf_preprocessed.fillna(0).drop(columns=["file_path"]).values
+                raise ValueError(
+                    f"Selected montage '{selected_montage}' is not available"
+                )
+            edf_preprocessed = eeg_montage.compute_differential_signals(
+                edf_csv, montage
+            )
+            edf_preprocessed2 = (
+                edf_preprocessed.fillna(0).drop(columns=["file_path"]).values
+            )
 
             # make predictions
             data = torch.stack([torch.tensor(d).float() for d in edf_preprocessed2])
             data = data.view(data.size(0), 1, input_size)
             prediction = model(data)
-            probabilities = torch.softmax(prediction, dim=1)  # Softmax to get probabilities
-            predicted_classes = torch.argmax(probabilities, dim=1)  # Predicted class indices (0 or 1)
+            probabilities = torch.softmax(
+                prediction, dim=1
+            )  # Softmax to get probabilities
+            predicted_classes = torch.argmax(
+                probabilities, dim=1
+            )  # Predicted class indices (0 or 1)
 
             # add predictions to the preprocessed files
-            predicted_classes_series = pd.Series(predicted_classes.numpy(), name="predicted_class")
-            edf_preprocessed_with_classes = edf_preprocessed.assign(predicted_class=predicted_classes_series)
-            predictions = edf_preprocessed_with_classes.head(10).to_dict(orient="records")
+            predicted_classes_series = pd.Series(
+                predicted_classes.numpy(), name="predicted_class"
+            )
+            edf_preprocessed_with_classes = edf_preprocessed.assign(
+                predicted_class=predicted_classes_series
+            )
+            predictions = edf_preprocessed_with_classes.head(10).to_dict(
+                orient="records"
+            )
 
             filename_processed = f"{file.split('.')[0]}_processed.csv"
             processed_path = os.path.join(app.config["DATA_FOLDER"], filename_processed)
             edf_preprocessed_with_classes.to_csv(processed_path, index=False)
 
-            additional_info.append({
-                "file_name": file,
-                "processed_file_name": f"{file.split('.')[0]}_processed.csv",
-                "duration": 10.5,  # Example duration
-            })
+            additional_info.append(
+                {
+                    "file_name": file,
+                    "processed_file_name": f"{file.split('.')[0]}_processed.csv",
+                    "duration": 10.5,  # Example duration
+                }
+            )
 
-        session['predictions'] = predictions
-        session['additional_info'] = additional_info
-        return render_template("predictions.html", predictions=predictions, additional_info=additional_info)
+        session["predictions"] = predictions
+        session["additional_info"] = additional_info
+        return render_template(
+            "predictions.html", predictions=predictions, additional_info=additional_info
+        )
     else:
-        predictions = session.get('predictions')
-        additional_info = session.get('additional_info')
+        predictions = session.get("predictions")
+        additional_info = session.get("additional_info")
         if predictions and additional_info:
-            return render_template("predictions.html", predictions=predictions, additional_info=additional_info)
+            return render_template(
+                "predictions.html",
+                predictions=predictions,
+                additional_info=additional_info,
+            )
         else:
-            flash("No predictions available. Please upload a file and run the prediction first.")
-            return redirect(url_for('upload_eeg'))
+            flash(
+                "No predictions available. Please upload a file and run the prediction first."
+            )
+            return redirect(url_for("upload_eeg"))
+
 
 # Download Prediction CSV
 @app.route("/download/<path:filename>", methods=["GET"])
@@ -226,43 +263,94 @@ def download_file(filename):
     processed_filename = f"{filename.split('.')[0]}_processed.csv"
     return send_from_directory(data_folder, processed_filename, as_attachment=True)
 
-
+# Altair Visual
 @app.route("/visual/<filename>")
 def visual(filename):
     processed_filename = f"data/{filename}"
     data = pd.read_csv(processed_filename)
     df = pd.DataFrame(data)
-    
-    eeg_columns = ['FP1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'FP2-F8', 'F8-T4', 'T4-T6', 'T6-O2',
-                'A1-T3', 'T3-C3', 'C3-CZ', 'CZ-C4', 'C4-T4', 'T4-A2', 'FP1-F3', 'F3-C3',
-                'C3-P3', 'P3-O1', 'FP2-F4', 'F4-C4', 'C4-P4', 'P4-O2']
 
-    colors = {channel: "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for channel in eeg_columns}
+    eeg_columns = [
+        "FP1-F7",
+        "F7-T3",
+        "T3-T5",
+        "T5-O1",
+        "FP2-F8",
+        "F8-T4",
+        "T4-T6",
+        "T6-O2",
+        "A1-T3",
+        "T3-C3",
+        "C3-CZ",
+        "CZ-C4",
+        "C4-T4",
+        "T4-A2",
+        "FP1-F3",
+        "F3-C3",
+        "C3-P3",
+        "P3-O1",
+        "FP2-F4",
+        "F4-C4",
+        "C4-P4",
+        "P4-O2",
+    ]
 
-    df_filtered = df[['timestamp'] + eeg_columns + ['predicted_class']]
-    df_melted = df_filtered.melt(id_vars=['timestamp'], var_name='Channel', value_name='Amplitude')
+    colors = {
+        channel: "#{:02x}{:02x}{:02x}".format(
+            random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+        )
+        for channel in eeg_columns
+    }
 
-    eeg_chart = alt.Chart(df_melted).mark_line().encode(
-        x=alt.X('timestamp', title='Timestamp (seconds)', axis=alt.Axis(titleFontSize=16, labelFontSize=14)),  # Adjust x-axis title and label font size
-        y=alt.Y('Amplitude', title='Amplitude', axis=alt.Axis(titleFontSize=16, labelFontSize=14)),  # Adjust y-axis title and label font size
-        color=alt.Color('Channel', scale=alt.Scale(domain=eeg_columns, range=[colors[ch] for ch in eeg_columns]), 
-                        legend=alt.Legend(title="EEG Channels", titleFontSize=16, labelFontSize=14)),
-        tooltip=['timestamp', 'Channel', 'Amplitude']
-    ).properties(
-        width=1000,
-        height=500,
-    ).interactive()
+    df_filtered = df[["timestamp"] + eeg_columns + ["predicted_class"]]
+    df_melted = df_filtered.melt(
+        id_vars=["timestamp"], var_name="Channel", value_name="Amplitude"
+    )
 
-    seizure_chart = alt.Chart(df_filtered[df_filtered['predicted_class'] == 1]).mark_rule(
-        color='pink',
-        opacity=0.075
-    ).encode(
-        x='timestamp',
-        size=alt.value(2)
+    eeg_chart = (
+        alt.Chart(df_melted)
+        .mark_line()
+        .encode(
+            x=alt.X(
+                "timestamp",
+                title="Timestamp (seconds)",
+                axis=alt.Axis(titleFontSize=16, labelFontSize=14),
+            ), 
+            y=alt.Y(
+                "Amplitude",
+                title="Amplitude",
+                axis=alt.Axis(titleFontSize=16, labelFontSize=14),
+            ),  
+            color=alt.Color(
+                "Channel",
+                scale=alt.Scale(
+                    domain=eeg_columns, range=[colors[ch] for ch in eeg_columns]
+                ),
+                legend=alt.Legend(
+                    title="EEG Channels", titleFontSize=16, labelFontSize=14
+                ),
+            ),
+            tooltip=["timestamp", "Channel", "Amplitude"],
+        )
+        .properties(
+            width=1000,
+            height=500,
+        )
+        .interactive()
+    )
+
+    seizure_chart = (
+        alt.Chart(df_filtered[df_filtered["predicted_class"] == 1])
+        .mark_rule(color="pink", opacity=0.075)
+        .encode(x="timestamp", size=alt.value(2))
     )
 
     combined_chart = alt.layer(eeg_chart, seizure_chart).interactive()
 
-    chart_json = combined_chart.to_json()  # Serialize the chart to JSON
-    return render_template("eeg_chart.html", title="EEG Data Visualization", chart_json=chart_json, filename=filename)
-
+    chart_json = combined_chart.to_json()  
+    return render_template(
+        "eeg_chart.html",
+        title="EEG Data Visualization",
+        chart_json=chart_json,
+        filename=filename,
+    )
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -9,114 +9,114 @@ class EEGMontage:
     def __init__(self):
         # 01_tcp_ar montage
         self.montage_pairs_01_tcp_ar = [
-            ('EEG FP1-REF', 'EEG F7-REF'),
-            ('EEG F7-REF', 'EEG T3-REF'),
-            ('EEG T3-REF', 'EEG T5-REF'),
-            ('EEG T5-REF', 'EEG O1-REF'),
-            ('EEG FP2-REF', 'EEG F8-REF'),
-            ('EEG F8-REF', 'EEG T4-REF'),
-            ('EEG T4-REF', 'EEG T6-REF'),
-            ('EEG T6-REF', 'EEG O2-REF'),
-            ('EEG A1-REF', 'EEG T3-REF'),
-            ('EEG T3-REF', 'EEG C3-REF'),
-            ('EEG C3-REF', 'EEG CZ-REF'),
-            ('EEG CZ-REF', 'EEG C4-REF'),
-            ('EEG C4-REF', 'EEG T4-REF'),
-            ('EEG T4-REF', 'EEG A2-REF'),
-            ('EEG FP1-REF', 'EEG F3-REF'),
-            ('EEG F3-REF', 'EEG C3-REF'),
-            ('EEG C3-REF', 'EEG P3-REF'),
-            ('EEG P3-REF', 'EEG O1-REF'),
-            ('EEG FP2-REF', 'EEG F4-REF'),
-            ('EEG F4-REF', 'EEG C4-REF'),
-            ('EEG C4-REF', 'EEG P4-REF'),
-            ('EEG P4-REF', 'EEG O2-REF')
+            ("EEG FP1-REF", "EEG F7-REF"),
+            ("EEG F7-REF", "EEG T3-REF"),
+            ("EEG T3-REF", "EEG T5-REF"),
+            ("EEG T5-REF", "EEG O1-REF"),
+            ("EEG FP2-REF", "EEG F8-REF"),
+            ("EEG F8-REF", "EEG T4-REF"),
+            ("EEG T4-REF", "EEG T6-REF"),
+            ("EEG T6-REF", "EEG O2-REF"),
+            ("EEG A1-REF", "EEG T3-REF"),
+            ("EEG T3-REF", "EEG C3-REF"),
+            ("EEG C3-REF", "EEG CZ-REF"),
+            ("EEG CZ-REF", "EEG C4-REF"),
+            ("EEG C4-REF", "EEG T4-REF"),
+            ("EEG T4-REF", "EEG A2-REF"),
+            ("EEG FP1-REF", "EEG F3-REF"),
+            ("EEG F3-REF", "EEG C3-REF"),
+            ("EEG C3-REF", "EEG P3-REF"),
+            ("EEG P3-REF", "EEG O1-REF"),
+            ("EEG FP2-REF", "EEG F4-REF"),
+            ("EEG F4-REF", "EEG C4-REF"),
+            ("EEG C4-REF", "EEG P4-REF"),
+            ("EEG P4-REF", "EEG O2-REF"),
         ]
 
         # 02_tcp_le montage
         self.montage_pairs_02_tcp_le = [
-            ('EEG FP1-LE', 'EEG F7-LE'),
-            ('EEG F7-LE', 'EEG T3-LE'),
-            ('EEG T3-LE', 'EEG T5-LE'),
-            ('EEG T5-LE', 'EEG O1-LE'),
-            ('EEG FP2-LE', 'EEG F8-LE'),
-            ('EEG F8-LE', 'EEG T4-LE'),
-            ('EEG T4-LE', 'EEG T6-LE'),
-            ('EEG T6-LE', 'EEG O2-LE'),
-            ('EEG A1-LE', 'EEG T3-LE'),
-            ('EEG T3-LE', 'EEG C3-LE'),
-            ('EEG C3-LE', 'EEG CZ-LE'),
-            ('EEG CZ-LE', 'EEG C4-LE'),
-            ('EEG C4-LE', 'EEG T4-LE'),
-            ('EEG T4-LE', 'EEG A2-LE'),
-            ('EEG FP1-LE', 'EEG F3-LE'),
-            ('EEG F3-LE', 'EEG C3-LE'),
-            ('EEG C3-LE', 'EEG P3-LE'),
-            ('EEG P3-LE', 'EEG O1-LE'),
-            ('EEG FP2-LE', 'EEG F4-LE'),
-            ('EEG F4-LE', 'EEG C4-LE'),
-            ('EEG C4-LE', 'EEG P4-LE'),
-            ('EEG P4-LE', 'EEG O2-LE')
+            ("EEG FP1-LE", "EEG F7-LE"),
+            ("EEG F7-LE", "EEG T3-LE"),
+            ("EEG T3-LE", "EEG T5-LE"),
+            ("EEG T5-LE", "EEG O1-LE"),
+            ("EEG FP2-LE", "EEG F8-LE"),
+            ("EEG F8-LE", "EEG T4-LE"),
+            ("EEG T4-LE", "EEG T6-LE"),
+            ("EEG T6-LE", "EEG O2-LE"),
+            ("EEG A1-LE", "EEG T3-LE"),
+            ("EEG T3-LE", "EEG C3-LE"),
+            ("EEG C3-LE", "EEG CZ-LE"),
+            ("EEG CZ-LE", "EEG C4-LE"),
+            ("EEG C4-LE", "EEG T4-LE"),
+            ("EEG T4-LE", "EEG A2-LE"),
+            ("EEG FP1-LE", "EEG F3-LE"),
+            ("EEG F3-LE", "EEG C3-LE"),
+            ("EEG C3-LE", "EEG P3-LE"),
+            ("EEG P3-LE", "EEG O1-LE"),
+            ("EEG FP2-LE", "EEG F4-LE"),
+            ("EEG F4-LE", "EEG C4-LE"),
+            ("EEG C4-LE", "EEG P4-LE"),
+            ("EEG P4-LE", "EEG O2-LE"),
         ]
 
         # 03_tcp_ar_a montage
-        # missing ('EEG A1-REF', 'EEG T3-REF'), ('EEG T4-REF', 'EEG A2-REF'), 
+        # missing ('EEG A1-REF', 'EEG T3-REF'), ('EEG T4-REF', 'EEG A2-REF'),
         # compared to 01_tcp_ar
         self.montage_pairs_03_tcp_ar_a = [
-            ('EEG FP1-REF', 'EEG F7-REF'),
-            ('EEG F7-REF', 'EEG T3-REF'),
-            ('EEG T3-REF', 'EEG T5-REF'),
-            ('EEG T5-REF', 'EEG O1-REF'),
-            ('EEG FP2-REF', 'EEG F8-REF'),
-            ('EEG F8-REF', 'EEG T4-REF'),
-            ('EEG T4-REF', 'EEG T6-REF'),
-            ('EEG T6-REF', 'EEG O2-REF'),
-            ('EEG T3-REF', 'EEG C3-REF'),
-            ('EEG C3-REF', 'EEG CZ-REF'),
-            ('EEG CZ-REF', 'EEG C4-REF'),
-            ('EEG C4-REF', 'EEG T4-REF'),
-            ('EEG FP1-REF', 'EEG F3-REF'),
-            ('EEG F3-REF', 'EEG C3-REF'),
-            ('EEG C3-REF', 'EEG P3-REF'),
-            ('EEG P3-REF', 'EEG O1-REF'),
-            ('EEG FP2-REF', 'EEG F4-REF'),
-            ('EEG F4-REF', 'EEG C4-REF'),
-            ('EEG C4-REF', 'EEG P4-REF'),
-            ('EEG P4-REF', 'EEG O2-REF')
+            ("EEG FP1-REF", "EEG F7-REF"),
+            ("EEG F7-REF", "EEG T3-REF"),
+            ("EEG T3-REF", "EEG T5-REF"),
+            ("EEG T5-REF", "EEG O1-REF"),
+            ("EEG FP2-REF", "EEG F8-REF"),
+            ("EEG F8-REF", "EEG T4-REF"),
+            ("EEG T4-REF", "EEG T6-REF"),
+            ("EEG T6-REF", "EEG O2-REF"),
+            ("EEG T3-REF", "EEG C3-REF"),
+            ("EEG C3-REF", "EEG CZ-REF"),
+            ("EEG CZ-REF", "EEG C4-REF"),
+            ("EEG C4-REF", "EEG T4-REF"),
+            ("EEG FP1-REF", "EEG F3-REF"),
+            ("EEG F3-REF", "EEG C3-REF"),
+            ("EEG C3-REF", "EEG P3-REF"),
+            ("EEG P3-REF", "EEG O1-REF"),
+            ("EEG FP2-REF", "EEG F4-REF"),
+            ("EEG F4-REF", "EEG C4-REF"),
+            ("EEG C4-REF", "EEG P4-REF"),
+            ("EEG P4-REF", "EEG O2-REF"),
         ]
 
         # 04_tcp_le_a
         # missing ('EEG A1-LE', 'EEG T3-LE'), ('EEG T4-LE', 'EEG A2-LE')
-        # compared to 02_tcp_le 
+        # compared to 02_tcp_le
         self.montage_pairs_04_tcp_le_a = [
-            ('EEG FP1-LE', 'EEG F7-LE'),
-            ('EEG F7-LE', 'EEG T3-LE'),
-            ('EEG T3-LE', 'EEG T5-LE'),
-            ('EEG T5-LE', 'EEG O1-LE'),
-            ('EEG FP2-LE', 'EEG F8-LE'),
-            ('EEG F8-LE', 'EEG T4-LE'),
-            ('EEG T4-LE', 'EEG T6-LE'),
-            ('EEG T6-LE', 'EEG O2-LE'),
-            ('EEG T3-LE', 'EEG C3-LE'),
-            ('EEG C3-LE', 'EEG CZ-LE'),
-            ('EEG CZ-LE', 'EEG C4-LE'),
-            ('EEG C4-LE', 'EEG T4-LE'),
-            ('EEG FP1-LE', 'EEG F3-LE'),
-            ('EEG F3-LE', 'EEG C3-LE'),
-            ('EEG C3-LE', 'EEG P3-LE'),
-            ('EEG P3-LE', 'EEG O1-LE'),
-            ('EEG FP2-LE', 'EEG F4-LE'),
-            ('EEG F4-LE', 'EEG C4-LE'),
-            ('EEG C4-LE', 'EEG P4-LE'),
-            ('EEG P4-LE', 'EEG O2-LE')
+            ("EEG FP1-LE", "EEG F7-LE"),
+            ("EEG F7-LE", "EEG T3-LE"),
+            ("EEG T3-LE", "EEG T5-LE"),
+            ("EEG T5-LE", "EEG O1-LE"),
+            ("EEG FP2-LE", "EEG F8-LE"),
+            ("EEG F8-LE", "EEG T4-LE"),
+            ("EEG T4-LE", "EEG T6-LE"),
+            ("EEG T6-LE", "EEG O2-LE"),
+            ("EEG T3-LE", "EEG C3-LE"),
+            ("EEG C3-LE", "EEG CZ-LE"),
+            ("EEG CZ-LE", "EEG C4-LE"),
+            ("EEG C4-LE", "EEG T4-LE"),
+            ("EEG FP1-LE", "EEG F3-LE"),
+            ("EEG F3-LE", "EEG C3-LE"),
+            ("EEG C3-LE", "EEG P3-LE"),
+            ("EEG P3-LE", "EEG O1-LE"),
+            ("EEG FP2-LE", "EEG F4-LE"),
+            ("EEG F4-LE", "EEG C4-LE"),
+            ("EEG C4-LE", "EEG P4-LE"),
+            ("EEG P4-LE", "EEG O2-LE"),
         ]
 
         # Create a dictionary for montage
         self.montage_dict = {
-            '01_tcp_ar': self.montage_pairs_01_tcp_ar,
-            '02_tcp_le': self.montage_pairs_02_tcp_le,
-            '03_tcp_ar_a': self.montage_pairs_03_tcp_ar_a,
-            '04_tcp_le_a': self.montage_pairs_04_tcp_le_a,
+            "01_tcp_ar": self.montage_pairs_01_tcp_ar,
+            "02_tcp_le": self.montage_pairs_02_tcp_le,
+            "03_tcp_ar_a": self.montage_pairs_03_tcp_ar_a,
+            "04_tcp_le_a": self.montage_pairs_04_tcp_le_a,
         }
 
     def read_edf_to_dataframe(self, file_path):
@@ -141,7 +141,7 @@ class EEGMontage:
                 time_axis = np.array([j / fs for j in range(num_samples)])
 
                 data[signal_label] = signal_data
-                data[f'timestamp_{signal_label}'] = time_axis
+                data[f"timestamp_{signal_label}"] = time_axis
 
                 time_axes.extend(time_axis)
 
@@ -152,24 +152,23 @@ class EEGMontage:
             common_timestamps = np.unique(np.round(time_axes, decimals=6))
 
             # Create a DataFrame to hold the common timestamps
-            df = pd.DataFrame({'timestamp': common_timestamps})
+            df = pd.DataFrame({"timestamp": common_timestamps})
 
             # Align each signal's data to the common timestamps
             for i in range(n):
                 signal_label = signal_labels[i]
-                time_axis = data.pop(f'timestamp_{signal_label}')
+                time_axis = data.pop(f"timestamp_{signal_label}")
                 signal_data = data.pop(signal_label)
 
                 # Create a DataFrame for the current signal
-                signal_df = pd.DataFrame({
-                    'timestamp': time_axis,
-                    signal_label: signal_data
-                })
+                signal_df = pd.DataFrame(
+                    {"timestamp": time_axis, signal_label: signal_data}
+                )
 
                 # Merge with the common timestamps DataFrame
-                df = pd.merge_asof(df, signal_df, on='timestamp', direction='nearest')
+                df = pd.merge_asof(df, signal_df, on="timestamp", direction="nearest")
 
-            df['file_path'] = file_path  # Add file path column
+            df["file_path"] = file_path  # Add file path column
             return df
         except Exception as e:
             print(f"An error occurred: {e}")
@@ -197,16 +196,17 @@ class EEGMontage:
                 column_name = f"{electrode_1.split()[1].split('-')[0]}-{electrode_2.split()[1].split('-')[0]}"  # Extract and format the desired column name
                 differential_df[column_name] = differential_signal
             else:
-                print(f"Warning: Columns {electrode_1} or {electrode_2} do not exist in the DataFrame and will be skipped.")
+                print(
+                    f"Warning: Columns {electrode_1} or {electrode_2} do not exist in the DataFrame and will be skipped."
+                )
 
-        for col in ['A1-T3', 'T4-A2', 'A1-T3', 'T4-A2']:
+        for col in ["A1-T3", "T4-A2", "A1-T3", "T4-A2"]:
             if col not in differential_df.columns:
                 differential_df[col] = 0
 
         # Append original columns from data to differential_df
-        for column in ['timestamp', 'file_path', 'label', 'confidence']:
+        for column in ["timestamp", "file_path", "label", "confidence"]:
             if column in data.columns:
                 differential_df[column] = data[column]
 
         return differential_df
-    
